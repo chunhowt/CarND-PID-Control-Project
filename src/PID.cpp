1b92de6d597ba8f72ca1f992971f135ce42450e0
Chun How Tan <chunhowt@users.noreply.github.com>
2017-05-11 13:16:21 +0800

    Implemented initial PID algorithm with a hardcoded hyperparameters manually tuned on a single track.

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,18 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double new_Kp, double new_Ki, double new_Kd) {
+  Kp = new_Kp;
+  Ki = new_Ki;
+  Kd = new_Kd;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte;
 }
 
 double PID::TotalError() {
+  return Kp * p_error + Kd * d_error + Ki * i_error;
 }
-
